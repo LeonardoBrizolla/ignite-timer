755eef20fdc1862745918104e6c190c51c61796b
Leonardo Brizolla <leonardobrizollars@gmail.com>
2023-09-07 17:14:31 -0300

    Header & Layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { History } from './pages/History'
-import { DefaultLauout } from './layouts/DefaultLayout'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLauout />}>
+      <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/history" element={<History />} />
       </Route>
